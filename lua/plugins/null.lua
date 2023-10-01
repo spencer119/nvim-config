7acc080210874a45e6e992d542cb64a307e7a764
Spencer Hamilton <spencernhamilton@gmail.com>
2023-10-01 00:28:00 -0600

    spencer-xps-arch @

---
+++ lua/plugins/null.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local nls = require("null-ls")
+      local format = nls.builtins.formatting
+      local diagnostics = nls.builtins.diagnostics
+      return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+        sources = {
+          format.stylua,
+          format.shfmt,
+        },
+      }
+    end,
+  },
+}
