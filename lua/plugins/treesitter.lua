9c64f930e9cbd02e051f70dc7dc439bbcd59b52c
Spencer Hamilton <spencernhamilton@gmail.com>
2023-10-05 10:20:55 -0600

    spencer-xps-arch @

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,6 +19,20 @@ return {
     end,
   },
   {
+    "luckasRanarison/tree-sitter-hypr",
+    config = function()
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.hypr = {
+        install_info = {
+          url = "https://github.com/luckasRanarison/tree-sitter-hypr",
+          files = { "src/parser.c" },
+          branch = "master",
+        },
+        filetype = "hypr",
+      }
+    end,
+  },
+  {
     "b0o/SchemaStore.nvim",
     lazy = true,
     version = false, -- last release is way too old
