a2c3e276c9d75365d9d35764f7cf8a66fd09bad5
Spencer Hamilton <spencernhamilton@gmail.com>
2023-10-05 18:33:03 -0600

    spencer-xps-arch @

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,11 +2,27 @@ return {
   {
     "williamboman/mason.nvim",
     opts = {
-      ensure_installed = {
-        "stylua",
-        "shfmt",
-        "clangd",
-      },
+      -- ensure_installed = {
+      --   "stylua",
+      --   "shfmt",
+      --   "clangd",
+      -- },
     },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "bashls",
+          "clangd",
+          "cmake",
+          "cssls",
+          "cssmodules_ls",
+          "dockerls",
+          "docker_compose_language_service",
+        },
+      })
+    end,
   },
 }
